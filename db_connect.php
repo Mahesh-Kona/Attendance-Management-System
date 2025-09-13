33c6487bbe26ad1fef1553dcf2fde7601f0557eb
Naga Mahesh Kona <royalkona4@gmail.com>
2025-09-13 16:27:29 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "attendance_management_system";
+$host = "sql102.infinityfree.com";  // MySQL Host Name
+$user = "if0_39931739";             // MySQL User Name
+$pass = "rguktn210163";     // Use your vPanel Password here
+$db   = "if0_39931739_attendance_management_system"; // MySQL DB Name
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $user, $pass, $db);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
