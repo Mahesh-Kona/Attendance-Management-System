62f6b0fe80f034d004f7902a4b3532fd8e94ace7
Naga Mahesh Kona <n210163@rguktn.ac.in>
2025-10-09 16:05:11 +0530

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -4,10 +4,10 @@
 // $pass = "rguktn210163";     // Use your vPanel Password here
 // $db   = "if0_39931739_attendance_management_system"; // MySQL DB Name
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "attendance_management_system";
+$servername = "byfmwzkfsnddf53znklo-mysql.services.clever-cloud.com";
+$username = "urj4gm5ldodmfe3p";
+$password = "X1EbFa5zIgJdkpvz9eaO";
+$dbname = "byfmwzkfsnddf53znklo";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
@@ -18,3 +18,4 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
