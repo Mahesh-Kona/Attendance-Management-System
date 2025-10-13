f68fb8616de318e35b3edd3388d76b343656da02
Naga Mahesh Kona <n210163@rguktn.ac.in>
2025-10-13 16:10:13 +0530

    Update all_faculty_info.php

--- all_faculty_info.php
+++ all_faculty_info.php
@@ -27,7 +27,6 @@ $result = $stmt->get_result();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>All Faculty Info</title>
     <style>
         table, th, td { border-collapse: collapse; padding: 8px; }
@@ -261,3 +260,4 @@ showPage(1);
 </script>
 </body>
 </html>
+
