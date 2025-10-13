94791d24a76dadf76517184c9779814370932392
Naga Mahesh Kona <n210163@rguktn.ac.in>
2025-10-13 16:15:41 +0530

    Update faculty_allotment_history.php

--- faculty_allotment_history.php
+++ faculty_allotment_history.php
@@ -85,7 +85,6 @@ $conn->close();
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Faculty Allotment History</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <style>
@@ -371,3 +370,4 @@ showPage(1);
 </script>
 </body>
 </html>
+
